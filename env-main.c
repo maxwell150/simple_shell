6fa812712f5a879527d6c02fa67414ba05fb56e5
JMukuni <bossjefter93@gmail.com>
2021-08-25 04:33:06 -0700

    Fixing indentation

--- env-main.c
+++ env-main.c
@@ -1,5 +1,4 @@
 #include "shell.h"
-
 /**
  * lookforslash - identifies if first char is a slash.
  * @cmd: first string
@@ -8,20 +7,17 @@
 int lookforslash(char *cmd)
 {
 int cont = 0;
- 
 while (cmd[cont])
-  {
-    if (cmd[0] == '/')
-	{
-	  printf("%c\n", cmd[0]);
-	  return (1);
-	}
-
-    cont++;
-  }
- return (0);
+{
+if (cmd[0] == '/')
+{
+printf("%c\n", cmd[0]);
+return (1);
 }
-
+cont++;
+}
+return (0);
+}
 /**
  * compareExit - identifies if first char is a slash.
  * @s1: first string
@@ -30,17 +26,15 @@ while (cmd[cont])
  */
 int compareExit(char *s1, char *s2)
 {
-  int i = 0;
-
-  for (; (*s2 != '\0' && *s1 != '\0') && *s1 == *s2; s1++)
-    {
-      if (i == 3)
+int i = 0;
+for (; (*s2 != '\0' && *s1 != '\0') && *s1 == *s2; s1++)
+{
+if (i == 3)
 	break;
-      i++;
-      s2++;
-    }
-
-  return (*s1 - *s2);
+i++;
+s2++;
+}
+return (*s1 - *s2);
 }
 /**
  * compareEnv - identifies if first char is a slash.
@@ -50,17 +44,15 @@ int compareExit(char *s1, char *s2)
  */
 int compareEnv(char *s1, char *s2)
 {
-  int i = 0;
-
-  for (; (*s2 != '\0' && *s1 != '\0') && *s1 == *s2; s1++)
-    {
-      if (i == 2)
-	break;
-      i++;
-      s2++;
-    }
-
-  return (*s1 - *s2);
+int i = 0;
+for (; (*s2 != '\0' && *s1 != '\0') && *s1 == *s2; s1++)
+{
+if (i == 2)
+break;
+i++;
+s2++;
+}
+return (*s1 - *s2);
 }
 /**
  * identify_string - identyfy keyboard input.
@@ -69,23 +61,19 @@ int compareEnv(char *s1, char *s2)
  **/
 char **identify_string(char *parameter)
 {
-  char **buf = malloc(1024 * sizeof(char *));
-  char *split;
-  int i = 0;
-  char *delim = " \t\n";
-
-
-  split = strtok(parameter, delim);
-
-  while (split != NULL)
-    {
-      buf[i] = split;
-      i++;
-      split = strtok(NULL, delim);
-    }
-  execute_proc(buf);
-  return (buf);
-
+char **buf = malloc(1024 * sizeof(char *));
+char *split;
+int i = 0;
+char *delim = " \t\n";
+split = strtok(parameter, delim);
+while (split != NULL)
+{
+buf[i] = split;
+i++;
+split = strtok(NULL, delim);
+}
+execute_proc(buf);
+return (buf);
 }
 /**
  * controlD - avoid close the shell
@@ -93,6 +81,6 @@ char **identify_string(char *parameter)
  **/
 void  controlD(int sig)
 {
-  (void) sig;
-  write(1, "\n$ ", 3);
+(void) sig;
+write(1, "\n$ ", 3);
 }
